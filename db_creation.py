eac2be4b1faac61eb12c6663be688a6b090fd3dd
100472310 <94962763+ManuGPR@users.noreply.github.com>
2023-09-10 18:26:18 +0200

    Add files via upload

--- db_creation.py
+++ db_creation.py
@@ -3,10 +3,26 @@ import sqlite3 as sql
 con = sql.connect("base_de_datos.db")
 cur = con.cursor()
 
-cur.execute("DROP TABLE usuario")
-cur.execute("CREATE TABLE usuario (usuario TEXT, contraseña TEXT, PRIMARY KEY(usuario))")
+cur.execute("PRAGMA foreign_keys = ON;") 
 
-cur.execute("INSERT INTO usuario Values('a', '1')")
+cur.execute("DROP TABLE operaciones")
+cur.execute("DROP TABLE balance")
+cur.execute("DROP TABLE usuarios")
+
+cur.execute("CREATE TABLE usuarios (usuario TEXT, contraseña TEXT, PRIMARY KEY(usuario))")
+cur.execute("CREATE TABLE balance (usuario TEXT, balance NUMBER, PRIMARY KEY(usuario), FOREIGN KEY (usuario) references usuarios(usuario))")
+cur.execute("CREATE TABLE operaciones (usuario TEXT, id NUMBER, tipo TEXT, fecha DATE, concepto TEXT, PRIMARY KEY(usuario, id), FOREIGN KEY (usuario) references usuarios(usuario))")
+
+
+cur.execute("INSERT INTO usuarios Values('a', 1)")
+cur.execute("INSERT INTO balance Values('a', 200)")
+cur.execute("INSERT INTO operaciones Values ('a', 1, 'R', 10/09/2023, 'Cena')")
+cur.execute("INSERT INTO operaciones Values ('a', 2, 'R', 10/09/2023, 'Cena')")
 con.commit()
-res = cur.execute("SELECT * from usuario")
-print(res.fetchall())
+
+res = cur.execute("SELECT * from usuarios")
+print("Usuarios: " + str(res.fetchall()))
+res = cur.execute("SELECT * from balance")
+print("Balance: " + str(res.fetchall()))
+res = cur.execute("Select * from operaciones")
+print("Operaciones: " + str(res.fetchall()))
