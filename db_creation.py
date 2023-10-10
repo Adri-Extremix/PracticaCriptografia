2b230d8e2aaea2d0f5d7a69a9dbca12006f93e5f
100472310 <94962763+ManuGPR@users.noreply.github.com>
2023-10-10 14:47:02 +0200

    upsi

--- db_creation.py
+++ db_creation.py
@@ -1,23 +1,23 @@
 import sqlite3 as sql
-
-con = sql.connect("PracticaCriptografia-main/base_de_datos.db")
+con = sql.connect("base_de_datos.db")
 cur = con.cursor()
-
 cur.execute("PRAGMA foreign_keys = ON;") 
 
-cur.execute("CREATE TABLE usuarios (usuario TEXT, contraseña TEXT, PRIMARY KEY(usuario))")
+try:
+    cur.execute("DROP TABLE operaciones")
+    cur.execute("DROP TABLE balance")
+    cur.execute("DROP TABLE usuarios")
+except:
+    pass
+
+cur.execute("CREATE TABLE usuarios (usuario TEXT, pwd_token TEXT, salt TEXT, PRIMARY KEY(usuario))")
 cur.execute("CREATE TABLE balance (usuario TEXT, balance NUMBER, PRIMARY KEY(usuario), FOREIGN KEY (usuario) references usuarios(usuario))")
 cur.execute("CREATE TABLE operaciones (usuario TEXT, id NUMBER, dinero NUMBER, tipo TEXT, fecha DATE, concepto TEXT, PRIMARY KEY(usuario, id), FOREIGN KEY (usuario) references usuarios(usuario))")
-
-
+"""    
 cur.execute("INSERT INTO usuarios Values('a', 1)")
 cur.execute("INSERT INTO balance Values('a', 200)")
-cur.execute("INSERT INTO operaciones Values ('a', 1, 200, 'R', '10/09/2023', 'Cena')")
+cur.execute("INSERT INTO operaciones Values ('a', 1, 100, 'R', '10/09/2023', 'Cena')")
+cur.execute("INSERT INTO operaciones Values ('a', 2, 200, 'R', '10/09/2023', 'Cena')")
+""" 
 con.commit()
-
-res = cur.execute("SELECT * from usuarios")
-print("Usuarios: " + str(res.fetchall()))
-res = cur.execute("SELECT * from balance")
-print("Balance: " + str(res.fetchall()))
-res = cur.execute("Select * from operaciones")
-print("Operaciones: " + str(res.fetchall()))
+con.close()
