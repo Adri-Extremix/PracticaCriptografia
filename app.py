be6b22a1a2ceacb54f9d59c823755836489266dd
100472182 <124138694+100472182@users.noreply.github.com>
2023-10-21 14:22:14 +0200

    Todo hecho parte 1

---
+++ app.py
@@ -0,0 +1,54 @@
+import tkinter as tk
+from . import window
+###Frame app
+frm_app = tk.Frame(master=window.window, width=500, height=500, bg="#d3d3d0")
+frm_app.pack_propagate(0)
+
+#Título de la app y widgets del login
+label_title = tk.Label(master=frm_app, text="Balance", fg="#000001", font=('Arial', 20, "bold"), bg="Blue")
+label_title.pack(side="top", pady=60, ipady=15, ipadx=250)
+
+
+app_deposit_button = tk.Button(
+    master=frm_app, 
+    text="Ingresar",
+    width=10,
+    height=2,
+    bg="#4CBD49",
+    fg="black",
+    )
+
+app_deposit_button.place(x=135, y = 400)
+
+app_withdraw_button = tk.Button(
+    master=frm_app, 
+    text="Retirada",
+    width=10,
+    height=2,
+    bg="red",
+    fg="black",
+    )
+app_withdraw_button.place(x=285, y=400)
+
+app_close_button = tk.Button(
+    master=frm_app, 
+    text="Cerrrar sesión",
+    width=10,
+    height=2,
+    bg="white",
+    fg="red",
+    )
+app_close_button.place(x=385,y= 10)
+
+app_record_button = tk.Button(
+    master = frm_app,
+    text = "Historial",
+    width= 10,
+    height=2,
+    bg = "white",
+    fg = "Blue",
+    )
+app_record_button.place(x= 210, y = 325)
+
+label_app_balance = tk.Label(master=frm_app, text="Placeholder", fg="#000001", font=('Arial', 20, "bold"), bg = "#D3D3D0")
+label_app_balance.pack(side="top", pady=40, ipady=15, ipadx=250)
---
+++ historial.py
@@ -0,0 +1,31 @@
+import tkinter as tk
+from tkinter import ttk
+from . import window
+###Frame historial
+frm_record = tk.Frame(master=window.window, width=500, height=500, bg="#d3d3d0")
+frm_record.pack_propagate(0)
+
+#Título de la app y widgets del login
+label_record = tk.Label(master=frm_record, text="Historial de operaciones", fg="#000001", font=('Arial', 20, "bold"), bg="Blue")
+label_record.pack(side="top", pady=60, ipady=15, ipadx=250)
+
+
+record_close_button = tk.Button(
+    master=frm_record, 
+    text="Atrás",
+    width=10,
+    height=2,
+    bg="white",
+    fg="red",
+    )
+
+record_close_button.place(x=385, y = 10)
+
+listbox_record = tk.Listbox()
+
+scroll_record = ttk.Scrollbar(master=frm_record, orient=tk.VERTICAL)
+listbox_record = tk.Listbox(master=frm_record, yscrollcommand=scroll_record.set, width=450, height = 150)
+scroll_record.config(command=listbox_record.yview)
+
+scroll_record.pack(side=tk.RIGHT, fill=tk.Y)
+listbox_record.pack()
